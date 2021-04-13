e820f7d79fbaf06fc96e565301af789eca0d054b
Shenxuan Wang <swang282@my.bcit.ca>
2021-04-12 19:19:14 -0700

    added wishlist button and push needed data

--- src/screens/ProductDetailScreen/ProductDetailScreen.js
+++ src/screens/ProductDetailScreen/ProductDetailScreen.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ScrollView, Button} from 'react-native';
 import styles from './styles';
+import firebase from 'firebase';
 
 export default function ProductDetailScreen(props) {
     const [product, setProduct] = useState(props.route.params);
+    const [uid, setUid] = useState('');
+    const db = firebase.firestore();
+
+    useEffect(() => {
+        const currUser = firebase.auth().currentUser;
+        setUid(currUser.uid);
+    }, []);
+
+    const handleWishList = async () => {
+        await db
+            .collection('users')
+            .doc(uid)
+            .collection('wishList')
+            .doc(product.name)
+            .set({
+                name: product.name,
+                price: product.price,
+                weight: product.weight,
+                image: product.image,
+                nutrition: product.nutrition,
+            }, {merge: true})
+            .then(alert('Product added to wish list!'));
+    };
 
     return (
         <View style={styles.root}>
@@ -16,6 +39,11 @@ export default function ProductDetailScreen(props) {
             <Text style={{color: 'black', fontSize: 20, margin: 20}}>Ingredient: </Text>           
             <Text style={styles.desc}>{product.nutrition}</Text>
             </ScrollView>
+            <Button
+                onPress={handleWishList}
+                title="Add to Wish List"
+                color="#F6AAB2"
+            />            
         </View>
     );
 }
